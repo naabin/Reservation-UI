a19ddf0db8c5d0a0e0d6f997e73009ccb4b01d28
Nabin Karki <naabin@outlook.com>
2020-01-10 13:10:53 +0545

    Reservation model created. Closes #10

---
+++ src/models/reservation.ts
@@ -0,0 +1,9 @@
+export class Reservation {
+    id: number;
+    fullName: string;
+    email: string;
+    phoneNumber: string;
+    numberOfPeople: string;
+    date: Date;
+    specialRequest: string;
+}
