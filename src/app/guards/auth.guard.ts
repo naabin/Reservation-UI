50f41954e47b8c6655689ec58f9336ba833dbd74
Nabin Karki <naabin@outlook.com>
2020-01-28 23:18:12 +1100

    Added alert service and authguard. Closes #12

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { UserSerivice } from '../services/user-service/user-serivice.service';
+
+
+@Injectable({'providedIn': 'root'})
+export class AuthGuard implements CanActivate {
+    
+
+    constructor(private router: Router, private authService: UserSerivice){}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+        const currentUser = this.authService.getCurrentUserValue;
+
+        if(currentUser){
+            return true;
+        }
+
+        //not logged in so redirect with login page with return url
+        this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+        return false;
+    }
+
+    
+}
